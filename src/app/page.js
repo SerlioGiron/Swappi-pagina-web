be5f1d1f625a1553beb926ba1d49afe1333ea55f
SerlioGiron <serlio.giron@unitec.edu>
2025-02-09 16:51:43 -0600

    avanzando

--- src/app/page.js
+++ src/app/page.js
@@ -2,31 +2,81 @@
 import Image from "next/image";
 import {Button, Typography} from "./MTailwind";
 import {StickyNavbar} from "../components/StickyNavbar";
+import Marquee from "react-fast-marquee";
+import {CarouselDefault} from "../components/Como-funciona";
+import {SimpleFooter} from "../components/Footer";
 
 export default function Home() {
     return (
         <div>
-            <StickyNavbar />
-            <div style={{height: "50vh", position: "relative"}}>
+          <StickyNavbar />
+            <div
+                style={{
+                  height: "90vh",
+                  position: "relative",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  flexDirection: "column",
+                  gap: "20px",
+                }}
+            >
                 <Image
-                    src="/assets/messi-1805.jpg"
+                    src="/assets/background.avif"
                     alt="Background"
                     layout="fill"
                     objectFit="cover"
                     quality={100}
-                    style={{opacity: 0.2}}
+                    style={{opacity: 0.2, zIndex: -1}}
                 />
                 <Typography
+                    variant="h1"
+                    color="black"
+                    style={{zIndex: 1, paddingTop: "30px"}}
+                >
+                    Intercambia, reutiliza y ahorra
+                </Typography>
+                <Typography variant="h3" color="black" style={{zIndex: 1}}>
+                    solo con swappi
+                </Typography>
+                <Button style={{zIndex: 1}}>Conoce mas</Button>
+
+                <div
                     style={{
-                        position: "absolute",
-                        top: "50%",
-                        left: "50%",
-                        transform: "translate(-50%, -50%)",
+                        display: "flex",
+                        flexDirection: "row",
+                        gap: "20px",
+                        width: "100%",
                     }}
                 >
-                    hola
-                </Typography>
+                    <Marquee speed={50} gradient={false} loop={0}>
+                        {[
+                            "Material Académico",
+                            "Tecnología",
+                            "Ropa y Accesorios",
+                            "Artículos para el Hogar",
+                            "Entretenimiento y Ocio",
+                            "Deportes y Salud",
+                            "Belleza y Cuidado Personal",
+                            "Herramientas y Otros",
+                        ].map((text, index) => (
+                            <Typography
+                                key={index}
+                                variant="h5"
+                                color="black"
+                                style={{
+                                    marginRight: "40px",
+                                    paddingTop: "30px",
+                                }}
+                            >
+                                {text}
+                            </Typography>
+                        ))}
+                    </Marquee>
+                </div>
             </div>
+            <CarouselDefault />
+            <SimpleFooter />
         </div>
     );
 }
---
+++ src/components/Como-funciona.jsx
@@ -0,0 +1,90 @@
+import {
+    Carousel,
+    Card,
+    CardHeader,
+    CardBody,
+    Typography,
+    Button,
+    IconButton
+} from "@material-tailwind/react";
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/outline";
+
+export function CarouselDefault() {
+    return (
+        <Carousel
+            className=""
+            prevArrow={({handlePrev}) => (
+                <IconButton
+                    variant="text"
+                    color="white"
+                    size="lg"
+                    onClick={handlePrev}
+                    className="!absolute left-4 top-1/2 -translate-y-1/2 bg-gray-900/50 hover:bg-gray-900 text-white"
+                >
+                    <ChevronLeftIcon className="h-6 w-6" />
+                </IconButton>
+            )}
+            nextArrow={({handleNext}) => (
+                <IconButton
+                    variant="text"
+                    color="white"
+                    size="lg"
+                    onClick={handleNext}
+                    className="!absolute right-4 top-1/2 -translate-y-1/2 bg-gray-900/50 hover:bg-gray-900 text-white"
+                >
+                    <ChevronRightIcon className="h-6 w-6" />
+                </IconButton>
+            )}
+            navigation={({setActiveIndex, activeIndex, length}) => (
+                <div className="absolute bottom-4 left-1/2 -translate-x-1/2 flex gap-2">
+                    {new Array(length).fill("").map((_, i) => (
+                        <span
+                            key={i}
+                            className={`block h-2 w-2 cursor-pointer rounded-full ${
+                                activeIndex === i ? "bg-white" : "bg-gray-500"
+                            }`}
+                            onClick={() => setActiveIndex(i)}
+                        />
+                    ))}
+                </div>
+            )}
+        >
+            {[{
+                title: "Paso 1: Regístrate con tu correo o cuenta universitaria",
+                image: "https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80"
+            },
+            {
+                title: "Paso 2: Sube los productos que quieres intercambiar",
+                image: "https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80"
+            },
+            {
+                title: "Paso 3: Encuentra productos y haz ofertas de intercambio",
+                image: "https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80"
+            },
+            {
+                title: "Paso 4: Confirma y completa el intercambio (con opciones de entrega segura) ",
+                image: "https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80"
+            },
+        ].map((value, index) => (
+                <Card key={index} className="w-[80%] h-[70vh] mx-auto flex flex-row items-center shadow-none">
+                    <CardHeader
+                        shadow={false}
+                        floated={false}
+                        className="w-full h-3/5"
+                    >
+                        <img
+                            src={value.image}
+                            alt="card-image"
+                            className="h-full w-full object-cover"
+                        />
+                    </CardHeader>
+                    <CardBody className="text-center">
+                        <Typography variant="h4" color="blue-gray" className="mb-2">
+                            {value.title}
+                        </Typography>
+                    </CardBody>
+                </Card>
+            ))}
+        </Carousel>
+    );
+}
